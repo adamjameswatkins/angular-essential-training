00245729e7c9ad87dacdddb3f7c670945966c316
Adam Watkins <adamjwatkins@gmail.com>
2022-04-04 20:18:00 +0100

    Using other components in a component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent }from "./app.component";
+import { MediaItemComponent } from "./media-item.component";
 
 @NgModule({
     imports: [
-        BrowserModule
+        BrowserModule        
     ],
-    declarations: [AppComponent],
+    declarations: [AppComponent, MediaItemComponent],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
