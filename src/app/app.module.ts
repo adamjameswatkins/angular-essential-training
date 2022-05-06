9c998cb0f50378eef98fe27d2655d3933af10da0
Adam Watkins <adamjwatkins@gmail.com>
2022-05-06 22:26:31 +0100

    Use custom directive
    Added Favorite directive to use for setting the is-favorite class on media items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
+import { FavoriteDirective } from "./favorite.directive";
 import { MediaItemListComponent } from "./media-item-list.component";
 import { MediaItemComponent } from "./media-item.component";
 
@@ -9,7 +10,12 @@ import { MediaItemComponent } from "./media-item.component";
     imports: [
         BrowserModule
     ],
-    declarations: [AppComponent, MediaItemComponent, MediaItemListComponent],
+    declarations: [
+        AppComponent,
+        MediaItemComponent,
+        MediaItemListComponent,
+        FavoriteDirective
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
