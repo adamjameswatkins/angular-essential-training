0c1332f5d918f3c3e9695e46607370cc1ec081fc
Adam Watkins <adamjwatkins@gmail.com>
2022-05-06 22:13:35 +0100

    Use ngFor
    Added MediaItemList and used in place of code in App.Component to show items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,8 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'mw-app',
     templateUrl: './app.component.html',
-    styleUrls: [`./app.component.css`]
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-    firstMediaItem = {
-        id: 1,
-        name: 'Firebug',
-        medium: 'Series',
-        category: 'Science Fiction',
-        year: 2010,
-        watchedOn: 1294166565384,
-        isFavourite: false
-    };
-
-    onMediaItemDelete(mediaItem) {
-        console.log(JSON.stringify(mediaItem));
-    }
-}
+export class AppComponent { }
